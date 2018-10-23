98c121063e78b9db4b3ee9b5ec2438595be7e585
carolinezhao <chaohsimeng@gmail.com>
2018-10-23 23:45:22 +0800

    notify subs

--- how-vue-works/observer.js
+++ how-vue-works/observer.js
@@ -1,19 +1,5 @@
 // https://segmentfault.com/a/1190000006599500
 
-var data = {
-    name: 'rabbit',
-    age: 16,
-    obj: {
-        a: 1,
-        b: 2
-    }
-};
-
-observe(data);
-data.name = 'caroline';
-data.age = 26;
-data.obj.b = 3;
-
 function observe(data) {
     if (!data || typeof data !== 'object') {
         return;
@@ -25,6 +11,8 @@ function observe(data) {
 };
 
 function defineReactive(data, key, value) {
+    var dep = new Dep();
+
     observe(value); // 监听子属性
     Object.defineProperty(data, key, {
         enumerable: true,
@@ -33,8 +21,38 @@ function defineReactive(data, key, value) {
             return value;
         },
         set(newValue) {
+            if (value === newValue) return;
             console.log(value, newValue);
             value = newValue;
+            dep.notify(); // 通知订阅者
         }
     });
 }
+
+function Dep() {
+    this.subs = []; // 订阅者
+}
+
+Dep.prototype.addSub = function (sub) {
+    this.subs.push(sub);
+}
+
+Dep.prototype.notify = function () {
+    console.log('notify!');
+    // this.subs.forEach(sub => sub.update());
+}
+
+var data = {
+    name: 'rabbit',
+    age: 16,
+    obj: {
+        a: 1,
+        b: 2
+    }
+};
+
+// 在定义函数之前调用，会导致 setter 中 dep 的原型链丢失
+observe(data);
+data.name = 'caroline';
+data.age = 26;
+data.obj.b = 3;
